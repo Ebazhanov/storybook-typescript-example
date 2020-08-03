fc105e934a5518a281b9bcbbc2c987534caad680
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2020-08-03 15:19:20 +0200

    - init 2

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,47 @@
+const path = require('path');
+const ROOT = path.resolve(__dirname, '../');
+const SRC = `${ROOT}/src`;
+const PATH_TO_SVG = `${SRC}/core/svg`;
+
 module.exports = {
-    stories: ['../src/**/*.stories.tsx']
+    stories: ['../src/**/*.stories.[tj]sx'],
+    webpackFinal: async (config) => {
+        const fileLoaderRule = config.module.rules.find(
+            (rule) => typeof rule.test.test === 'function' && rule.test.test('.svg'),
+        );
+        fileLoaderRule.exclude = PATH_TO_SVG;
+
+        config.module.rules.push(
+            {
+                test: /\.tsx?$/,
+                loader: require.resolve('babel-loader'),
+            },
+            {
+                test: /\.stories\.tsx?$/,
+                loaders: [
+                    {
+                        loader: require.resolve('@storybook/source-loader'),
+                        options: { parser: 'typescript' },
+                    },
+                ],
+                enforce: 'pre',
+            },
+            {
+                test: /\.svg$/,
+                include: PATH_TO_SVG,
+                use: [
+                    {
+                        loader: '@svgr/webpack',
+                        options: {
+                            icon: true,
+                        },
+                    },
+                ],
+            },
+        );
+
+        config.resolve.extensions.push('.ts', '.tsx');
+        config.resolve.modules.push(SRC, 'node_modules');
+        return config;
+    },
 };
---
+++ src/comonents/Button/Button.tsx
@@ -0,0 +1,9 @@
+import React, { memo } from 'react';
+
+function Button() {
+    return (
+        <div>AFASFASDSADASD</div>
+    );
+}
+
+export default memo(Button);
