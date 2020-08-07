e3e1dea1aca8592303ed565c45959f5333dde23a
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2020-08-07 15:50:39 +0200

    - updates

--- src/components/Button/styled.ts
+++ src/components/Button/styled.ts
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 
 export const ButtonWrapper = styled.button`
-    font-size: 100px;
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border-radius: 3px;
 `;
